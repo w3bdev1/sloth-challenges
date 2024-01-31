572dddff8690acb500dbffdb27f3d6aebf9d784d
Sudeep Biswas <w3bdev@protonmail.com>
2024-01-31 15:54:27 +0530

    Day1[Rust]: parse input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    match parse_input() {
+        Ok(input) => println!("{:?}", input),
+        Err(message) => println!("{}", message),
+    }
 }
+
+fn parse_input() -> Result<usize, String> {
+    let input = std::env::args().nth(1);
+
+    if let Some(n) = input {
+        match n.parse::<usize>() {
+            Ok(number) => return Ok(number),
+            Err(_) => return Err("Provide a valid and positive numeric input".to_string()),
+        }
+    } else {
+        return Err("Provide dollar value as first argument".to_string())
+    }
+}
+
